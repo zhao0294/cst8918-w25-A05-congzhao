35d334db6cb47c4034cf250149f43a1c35e34e74
zhao0294 <zhao0294@algonquinlive.com>
2025-06-05 21:22:30 -0400

    Add Resource Group

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "2.3.3"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "cloudinit" {}
+
+# -------------------
+# Resource Group
+# -------------------
+resource "azurerm_resource_group" "this" {
+  name     = "${var.labelPrefix}-A05-RG"
+  location = var.region
+}
