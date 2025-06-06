fdc9d0bc2a8590b631f97a96b5d1d7eeeeef7479
zhao0294 <zhao0294@algonquinlive.com>
2025-06-05 21:25:47 -0400

    Add subnet

--- main.tf
+++ main.tf
@@ -47,3 +47,13 @@ resource "azurerm_virtual_network" "this" {
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
+
+# -------------------
+# Subnet
+# -------------------
+resource "azurerm_subnet" "this" {
+  name                 = "${var.labelPrefix}-subnet"
+  resource_group_name  = azurerm_resource_group.this.name
+  virtual_network_name = azurerm_virtual_network.this.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
