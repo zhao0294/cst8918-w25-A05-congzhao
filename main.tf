8775a390d121f0517fc24b9c4811fd21b6e4c73d
zhao0294 <zhao0294@algonquinlive.com>
2025-06-05 21:27:38 -0400

    Add network interface

--- main.tf
+++ main.tf
@@ -57,3 +57,54 @@ resource "azurerm_subnet" "this" {
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.1.0/24"]
 }
+
+# -------------------
+# Network Security Group (NSG)
+# -------------------
+resource "azurerm_network_security_group" "this" {
+  name                = "${var.labelPrefix}-nsg"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# -------------------
+# Network Interface (NIC)
+# -------------------
+resource "azurerm_network_interface" "this" {
+  name                = "${var.labelPrefix}-nic"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.this.id
+  }
+
+  network_security_group_id = azurerm_network_security_group.this.id
+}
